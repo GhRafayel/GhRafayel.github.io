5144744e079726f2dd88d45631670da6e0b01a57
Rafayel Ghazaryan <rafayelghazaryan@MacBook-Pro-von-Rafayel.local>
2023-08-01 17:30:10 +0200

    Shopping_List/index.js,Shopping_List/public/script.js

--- Shopping_List/index.js
+++ Shopping_List/index.js
@@ -7,17 +7,17 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('https://ghrafayel.github.io/Shopping_List/public/index.html', (req, res) => {
-  req.redirect("../Shopping_List/public/index.html");
+app.get('/', (req, res) => {
+  req.redirect("public/index.html");
 });
 
-app.get('https://ghrafayel.github.io/Shopping_List/public/index.html', (req, res) => {
+app.get('/todo', (req, res) => {
   fs.promises.readFile(path.resolve('data.json'), 'utf-8').then(json =>{
     res.send(json);
     console.log(json);
   })
 })
-app.post('https://ghrafayel.github.io/Shopping_List/public/index.html', (req, res) => {
+app.post('/todo', (req, res) => {
   fs.promises
   .writeFile(path.resolve("data.json"),JSON.stringify(req.body,undefined,2))
   .then(json =>{
