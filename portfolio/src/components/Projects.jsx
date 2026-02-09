dbc69073ebce79de6824b467fe41b005602b5812
Rafayel Ghazaryan <rafayelghazaryan@MacBook-Pro-von-Rafayel.local>
2026-02-09 23:45:16 +0100

    css updata

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -96,7 +96,8 @@ const projects = [
 const Projects = ({darkMode}) => {
 	const colors = darkMode ? 'text-gray-100' : 'text-gray-900';
 	return (
-		<section id="projects" className={`${colors} relative px-24`}>
+		<section id="projects" className={`${colors} relative px-24 justify-center`}>
+			
 			<div className="container max-auto px-4 min-w-2xs">
 				<div className="text-center mb-10" data-aos='fade-up'>
 					<h2 className={`sm:text-4xl font-bold mb-3 ${darkMode ? 'white' : '#1f2937'}`} > My Project </h2>
@@ -106,45 +107,46 @@ const Projects = ({darkMode}) => {
 				</div>
 				
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-12">
-					{projects.map((project, index) => (
-				<div key={index} className={`${colors}`} data-aos="fade-up" data-aos-delay={`${300 + index * 100}`} >
-			<div className="h-full p-4 overflow-hidden rounded-2xl flex flex-col border border-green-600 hover:border-green-500/50 transition-all duration-300 hover:-translate-y-2">
-				{/* Image */}
-				<div className="w-full h-48 overflow-hidden rounded-t-xl">
-					<img src={project.image} alt={project.title} className="w-full h-full object-center" />
+					{projects.map((project, index) => 
+					(
+						<div key={index} className={`${colors}`} data-aos="fade-up" data-aos-delay={`${300 + index * 100}`} >
+							<div className="h-full p-4 overflow-hidden rounded-2xl flex flex-col border border-green-600 hover:border-green-500/50 transition-all duration-300 hover:-translate-y-2">
+								{/* Image */}
+								<div className="w-full h-48 overflow-hidden rounded-t-xl">
+									<img src={project.image} alt={project.title} className="w-full h-full object-center" />
+								</div>
+
+								{/* Content */}
+								<div className="p-4 flex flex-col flex-grow">
+									<h3 className={`${colors} text-lg font-bold mb-2`}> {project.title} </h3>
+
+									<p className={`${colors} text-sm mb-3 line-clamp-3`}> {project.desc} </p>
+
+									<div className={`${colors} flex flex-wrap gap-1.5 mb-4`}>
+										{project.tags.map((item, i) => (
+											<span key={i} className="px-3 py-1 text-xs rounded-full border border-gray-400 hover:border-green-600 transition">
+												{item}
+											</span>
+										))}
+									</div>
+
+									{/* Buttons */}
+									<div className="mt-auto flex gap-2">
+										<a href={project.href} target="_blank" className={`flex-1 flex items-center justify-center gap-1.5 px-3 py-2 text-sm rounded-lg bg-blue-400 ${colors} cursor-pointer`}>
+											<FaGithub />
+											Code
+										</a>
+										<a className="flex-1 flex items-center justify-center gap-1.5 px-3 py-2 text-sm rounded-lg bg-green-500">
+											<FaExternalLinkAlt />
+											Demo
+										</a>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					))}
 				</div>
-
-				{/* Content */}
-				<div className="p-4 flex flex-col flex-grow">
-					<h3 className={`${colors} text-lg font-bold mb-2`}> {project.title} </h3>
-
-					<p className={`${colors} text-sm mb-3 line-clamp-3`}> {project.desc} </p>
-
-					<div className={`${colors} flex flex-wrap gap-1.5 mb-4`}>
-						{project.tags.map((item, i) => (
-							<span key={i} className="px-3 py-1 text-xs rounded-full border border-gray-400 hover:border-green-600 transition">
-								{item}
-							</span>
-						))}
-					</div>
-
-					{/* Buttons */}
-					<div className="mt-auto flex gap-2">
-						<a href={project.href} target="_blank" className={`flex-1 flex items-center justify-center gap-1.5 px-3 py-2 text-sm rounded-lg bg-blue-400 ${colors} cursor-pointer`}>
-							<FaGithub />
-							Code
-						</a>
-						<a className="flex-1 flex items-center justify-center gap-1.5 px-3 py-2 text-sm rounded-lg bg-green-500">
-							<FaExternalLinkAlt />
-							Demo
-						</a>
-					</div>
-				</div>
-
-			</div>
-		</div>
-	))}
-</div>
 
 				<div className="text-center mt-10">
 						<a href="#" className={`inline-flex items-center font-semibold gap-2 px-7 py-4  
