d0625e7fbba3c0b79b8b9c24afb8f97907a989e4
GhRafayel <ghazarysnrafayel@gmail.com>
2026-08-29 23:53:15 +0200

    added component

---
+++ portfolio/src/components/todo.jsx
@@ -0,0 +1,70 @@
+ import { useState } from "react";
+ import Data from "./data";
+
+export default function Todo ({darkMode}) {
+    const [data, setData] = useState(Data.webProjects.todo);
+
+	const colors = darkMode ? 'text-gray-100' : 'text-gray-900';
+	const code_dome_buttons = `flex-1 flex items-center justify-center gap-1.5 px-3 py-2 text-sm rounded-lg transition-all duration-500 cursor-pointer ${darkMode ? "hover:text-gray-900" :"hover:text-white"}`;
+
+	return (
+		<section id="projects" className={`${colors} relative px-24 text-center`}>
+			
+			<div className="container mx-auto px-4">
+            
+                            <div className="text-center mb-10" data-aos='fade-up'>
+                                <h2 className={`sm:text-4xl font-bold mb-3 ${darkMode ? 'white' : '#1f2937'}`} > Web Projects </h2>
+                                <p className={`max-w-xl mx-auto ${darkMode ? '#d1d5db' : '#6b7280'}`}>
+                                    A show case of my recent projects.
+                                </p>
+                            </div>
+            
+                            <div className="grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-12`">
+                                {data.map((project, index) => 
+                                (
+                                    <div key={index} className={`${colors} justify-items-center`} data-aos="fade-up" data-aos-delay={`${300 + index * 100} min-w-70`} >
+                                        <div className="h-full  max-w-90 min-w-60 p-4 overflow-hidden rounded-2xl flex justify-between flex-col border border-green-600 hover:border-green-500/50 transition-all duration-300 hover:-translate-y-2">
+                                        
+                                            {/* Image */}
+                                            <div className="w-full  h-58 overflow-hidden rounded-t-xl">
+                                                <img src={project.image} alt={project.title} className="w-full h-full object-center" />
+                                            </div>
+            
+                                            {/* Content */}
+                                            <div className="p-4 flex flex-col">
+
+                                                <h3 className={`${colors} text-lg font-bold mb-2`}> {project.title} </h3>
+            
+                                                <p className={`${colors} text-sm mb-3 line-clamp-3`}> {project.desc} </p>
+            
+                                                <div className={`${colors} flex flex-wrap gap-1.5 mb-4`}>
+                                                    {data.map((item, i) => (
+                                                        <span key={i} className="px-3 py-1 text-xs rounded-full border border-gray-400 hover:border-green-600  hover:scale-110  transition-all duration-100">
+                                                            {item}
+                                                        </span>
+                                                    ))}
+                                                </div>
+                                                
+                                                {/* Buttons */}
+                                                <div className="mt-auto flex flex-col min-[450px]:flex-row gap-2 justify-center">
+                                                    <a href={project.href} target="_blank" className={`${code_dome_buttons} bg-blue-400`}> 
+                                                        <FaGithub />
+                                                        Code
+                                                    </a>
+            
+                                                    <a href={project.href} target="_blank" className={`${code_dome_buttons} bg-green-500`}>
+                                                        <FaExternalLinkAlt />
+                                                        Demo
+                                                    </a>
+                                                </div>
+
+                                            </div>
+            
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+		</section>
+	)
+}
